132374fa82a6bd550d7637166c1bac2c2221dadd
Adam Brewer <Adam.Brewer@datapath.co.uk>
2022-08-08 15:14:56 +0100

    not tests

---
+++ ComputerSimulator.Core.Tests/Gates/NotTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using ComputerSimulator.Core.Gates;
+using ComputerSimulator.Core.Parts;
+using ComputerSimulator.TestUtilities;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+
+namespace ComputerSimulator.Core.Tests.Gates;
+
+public class NotTests : MockBase<Not>
+{
+    [Test]
+    [TestCase(false)]
+    [TestCase(true)]
+    public void InputChanged_OutputIsReverseOfInput(bool input)
+    {
+        // Arrange
+        var inputWire = Mock.Of<IWire2<bool>>(m => m.Value == input);
+        
+        var sut = CreateSut();
+        sut.Input = inputWire;
+        sut.Output = Mock.Of<IWire2<bool>>();
+
+        // Act
+        Mock.Get(inputWire)
+            .Raise(m => m.ValueChanged += null, EventArgs.Empty);
+        
+        // Assert
+        sut.Output
+            .Value
+            .Should()
+            .Be(!input);
+    }
+}
