bddbc93ea0348dd47057be8188d316e4d9934268
Adam Brewer <Adam.Brewer@datapath.co.uk>
2022-08-15 09:29:46 +0100

    Removed unused classes

--- ComputerSimulator.Core/Parts/Wire2.cs
+++ ComputerSimulator.Core/Parts/Wire2.cs
@@ -10,69 +10,6 @@ public interface IWire2<T> : IWire2
     T Value { get; set; }
 }
 
-// public class MessageBrokerWire<T> : IWire2<T>, IMessageHandler<T>
-// {
-//     private bool _valueSet;
-//     private readonly IMessageBroker _messageBroker;
-//     private T _value;
-//     private string _label = string.Empty;
-//
-//     public MessageBrokerWire(IMessageBroker messageBroker, T initialValue)
-//     {
-//         _messageBroker = messageBroker;
-//         _value = initialValue;
-//     }
-//
-//     public event EventHandler? ValueChanged;
-//
-//     public string Label
-//     {
-//         get => _label;
-//         set
-//         {
-//             if (_label == value)
-//             {
-//                 return;
-//             }
-//             
-//             _messageBroker.ReplaceQueue<bool>(_label, value);
-//             _messageBroker.ReplaceHandler(_label, value, this);
-//             _label = value;
-//         }
-//     }
-//
-//     public T Value
-//     {
-//         get => _value;
-//         set
-//         {
-//             if (_valueSet && EqualityComparer<T>.Default.Equals(_value, value))
-//             {
-//                 return;
-//             }
-//
-//             _valueSet = true;
-//             _value = value;
-//             InternalValueChanged();
-//         }
-//     }
-//
-//     private void InternalValueChanged()
-//     {
-//         if (string.IsNullOrWhiteSpace(Label))
-//         {
-//             return;
-//         }
-//         
-//         _messageBroker.Publish(Label, Value);
-//     }
-//
-//     public void Handle(T message)
-//     {
-//         ValueChanged?.Invoke(this, EventArgs.Empty);
-//     }
-// }
-
 public class EventWire<T> : IWire2<T>
 {
     private T _value;
@@ -100,21 +37,6 @@ public class EventWire<T> : IWire2<T>
             ValueChanged?.Invoke(this, EventArgs.Empty);
         }
     }
-}
-
-public class DisconnectedWire<T> : IWire2<T>
-{
-    public event EventHandler? ValueChanged;
-
-    public string Label { get; set; } = string.Empty;
-
-    public T Value
-    {
-        get => default!;
-        set => throw new NotImplementedException();
-    }
-
-    public static IWire2<T> Instance => new DisconnectedWire<T>();
 }
 
 public static class WireHelper
