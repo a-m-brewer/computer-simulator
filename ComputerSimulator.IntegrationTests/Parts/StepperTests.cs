c477bfd2cbf1ec2c3fc6e89e9926c9ff47831484
Adam Brewer <Adam.Brewer@datapath.co.uk>
2022-09-13 16:01:43 +0100

    Add more stepper tests wip

--- ComputerSimulator.IntegrationTests/Parts/StepperTests.cs
+++ ComputerSimulator.IntegrationTests/Parts/StepperTests.cs
@@ -41,7 +41,86 @@ public class StepperTests : IntegrationTestBase
                 stepper.Steps[i].Value.Should().BeFalse($"Stepper {i} should be false");
             }
         }
-        
+
         stepper.Steps[onBit].Value.Should().BeTrue();
+    }
+
+    [Test]
+    public void CanResetTo0()
+    {
+        var stepper = ComponentFactory.CreateStepper(
+            CreateTestWire(false, "clk"),
+            CreateTestWire(false, "reset"),
+            CreateTestWireGroup(false, 7, "step"));
+
+        for (var i = 0; i < 8; i++)
+        {
+            stepper.Clk.Value = true;
+            stepper.Update();
+            stepper.Clk.Value = false;
+            stepper.Update();
+        }
+
+        stepper[]
+        
+        var output = stepperOutput;
+        var rest = stepperOutput.Where(w => output.IndexOf(w) != 6);
+        var restOn = rest.Any(w => w);
+        Assert.IsFalse(restOn);
+
+        stepper.Clk.Value = true;
+        stepper.Reset.Value = true;
+        stepper.Update();
+
+        Assert.IsTrue(stepperOutput[0]);
+        rest = stepperOutput.Where(w => stepperOutput.IndexOf(w) != 0);
+        restOn = rest.Any(w => w);
+        Assert.IsFalse(restOn);
+    }
+
+    [Test]
+    public void InfiniteStepper()
+    {
+        var stepper = TestUtils.CreateStepper();
+
+        var stepperOutput = new StepperOutput();
+
+        for (var i = 0; i < 8; i++)
+        {
+            stepper.Step(true, false);
+            stepperOutput = stepper.Step(false, false);
+        }
+
+        Assert.IsTrue(stepperOutput[6]);
+        var output = stepperOutput;
+        var rest = stepperOutput.Where(w => output.IndexOf(w) != 6);
+        var restOn = rest.Any(w => w);
+        Assert.IsFalse(restOn);
+
+        stepperOutput = stepper.Step(true);
+
+        Assert.IsTrue(stepperOutput[0]);
+        rest = stepperOutput.Where(w => stepperOutput.IndexOf(w) != 0);
+        restOn = rest.Any(w => w);
+        Assert.IsFalse(restOn);
+    }
+
+    [Test]
+    public void StaysAtIndex7IfNotReset()
+    {
+        var stepper = TestUtils.CreateStepper();
+
+        var stepperOutput = new StepperOutput();
+
+        for (var i = 0; i < 8; i++)
+        {
+            stepper.Step(true, false);
+            stepperOutput = stepper.Step(false, false);
+        }
+
+        Assert.IsTrue(stepperOutput[6]);
+        var rest = stepperOutput.Where(w => stepperOutput.IndexOf(w) != 6);
+        var restOn = rest.Any(w => w);
+        Assert.IsFalse(restOn);
     }
 }
