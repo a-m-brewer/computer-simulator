cebd376302ba89d7de6f652458de3784d92c4fed
Adam Brewer <adam@adambrewer.co.uk>
2022-09-17 10:44:29 +0100

    WIP cpu

---
+++ ComputerSimulator.Core/Extensions/ComponentExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace ComputerSimulator.Core.Extensions;
+
+public static class ComponentExtensions
+{
+    public static void Update(this IEnumerable<IComponent> components)
+    {
+        foreach (var component in components)
+        {
+            component.Update();
+        }
+    }
+}
--- ComputerSimulator.Core/Parts/CentralProcessingUnit.cs
+++ ComputerSimulator.Core/Parts/CentralProcessingUnit.cs
@@ -1,4 +1,6 @@
-﻿using ComputerSimulator.Core.Constants;
+﻿using ComputerSimulator.Core.Circuits;
+using ComputerSimulator.Core.Constants;
+using ComputerSimulator.Core.Extensions;
 using ComputerSimulator.Core.Factories;
 using ComputerSimulator.Core.Gates;
 
@@ -53,18 +55,27 @@ public class CentralProcessingUnit : PartsBase, ICentralProcessingUnit
     private readonly IStepper _stepper;
     private readonly IAnd2 _ramEnableAnd;
     private readonly IAnd2 _accEnableAnd;
-    private readonly IAnd2[] _generalPurposeRegistersEnableAnd;
+    private readonly IOr2[] _generalPurposeRegistersEnableOr;
     private readonly IAnd2 _marSetAnd;
     private readonly IAnd2 _accSetAnd;
     private readonly IAnd2 _ramSetAnd;
     private readonly IAnd2 _tmpSetAnd;
-    private readonly IAnd2[] _generalPurposeRegistersSetAnd;
     private readonly IAnd2 _iarEnableAnd;
     private readonly IAnd2 _irSetAnd;
     private readonly IAnd2 _iarSetAnd;
     private readonly IOr2 _marSetOr;
-    private readonly IOr2 _accSetOr;
-    private readonly IOr2 _accEnableOr;
+    private readonly IDecoder _registerBEnable2X4Decoder;
+    private readonly IDecoder _registerAEnable2X4Decoder;
+    private readonly IDecoder _registerBSet2X4Decoder;
+    private readonly IAnd[] _registerAEnableDecoderAnd;
+    private readonly IAnd[] _registerBEnableDecoderAnd;
+    private readonly IAnd[] _registerBSetDecoderAnd;
+    private readonly IAnd[] _aluOutputAnds;
+    private readonly IAnd _irAnd;
+    private readonly INot _irNot;
+    private readonly IAnd2 _irStep4And;
+    private readonly IAnd2 _irStep5And;
+    private readonly IAnd _irStep6And;
 
     public CentralProcessingUnit(
         IWire<bool> iarEnable,
@@ -104,29 +115,88 @@ public class CentralProcessingUnit : PartsBase, ICentralProcessingUnit
 
         _stepper = ComponentFactory.CreateStepper(_clock.Clk,
             WireFactory.CreateGroup(false, WireConstants.ExpectedNumberOfSteps, "step"));
+
+        _aluOutputAnds = 3
+            .InitArray<IAnd>()
+            .Fill(i => ComponentFactory.CreateAnd(
+                WireFactory.CreateGroup(
+                    StepWire(5),
+                    InstructionRegister[0],
+                    InstructionRegister[i + 1]),
+                Op[i]));
+
+        // Might become a NAND but the diagram did them as separate
+        _irAnd = ComponentFactory.CreateAnd(
+            WireFactory.CreateGroup(
+                InstructionRegister[1], InstructionRegister[2], InstructionRegister[3]),
+            WireFactory.CreateWire(false, "ir-and-output"));
+        _irNot = ComponentFactory.CreateNot(_irAnd.Output, WireFactory.CreateWire(false, "ir-not-output"));
+
+        _irStep4And = ComponentFactory.CreateAnd2(
+            StepWire(4),
+            InstructionRegister[0],
+            WireFactory.CreateWire(false, "ir-step-4-and-output"));
         
+        _irStep5And = ComponentFactory.CreateAnd2(
+            StepWire(5),
+            InstructionRegister[0],
+            WireFactory.CreateWire(false, "ir-step-5-and-output"));
+
+        _irStep6And = ComponentFactory.CreateAnd(
+            WireFactory.CreateGroup(
+                StepWire(6),
+                InstructionRegister[0],
+                _irNot.Output),
+            WireFactory.CreateWire(false, "ir-step-6-and-output"));
+        
+        // IR Decoders
+        _registerAEnable2X4Decoder = ComponentFactory.CreateDecoder(WireFactory.CreateGroup(InstructionRegister[4], InstructionRegister[5]));
+        var registerBAddress = WireFactory.CreateGroup(InstructionRegister[6], InstructionRegister[7]);
+        _registerBEnable2X4Decoder = ComponentFactory.CreateDecoder(registerBAddress);
+        _registerBSet2X4Decoder = ComponentFactory.CreateDecoder(registerBAddress);
+
+        _registerAEnableDecoderAnd = WireConstants.ExpectedNumberOfGeneralPurposeRegisters
+            .InitArray<IAnd>()
+            .Fill(i => ComponentFactory.CreateAnd(
+                WireFactory.CreateGroup(
+                    _clock.ClkE,
+                    _irStep5And.Output, // RegA
+                    _registerAEnable2X4Decoder.Outputs[i]),
+                WireFactory.CreateWire(false, "register-a-enable-decoder-and-output")));
+        
+        _registerBEnableDecoderAnd = WireConstants.ExpectedNumberOfGeneralPurposeRegisters
+            .InitArray<IAnd>()
+            .Fill(i => ComponentFactory.CreateAnd(
+                WireFactory.CreateGroup(
+                    _clock.ClkE,
+                    _irStep4And.Output, // RegB
+                    _registerBEnable2X4Decoder.Outputs[i]),
+                WireFactory.CreateWire(false, "register-b-enable-decoder-and-output")));
+        
+        _registerBSetDecoderAnd = WireConstants.ExpectedNumberOfGeneralPurposeRegisters
+            .InitArray<IAnd>()
+            .Fill(i => ComponentFactory.CreateAnd(
+                WireFactory.CreateGroup(
+                    _clock.ClkS,
+                    _irStep6And.Output, // RegB
+                    _registerBSet2X4Decoder.Outputs[i]),
+                GeneralPurposeRegistersSet[i]));
+
         // Enables
         _iarEnableAnd = ComponentFactory.CreateAnd2(_clock.ClkE, StepWire(1), IarEnable);
         _ramEnableAnd = ComponentFactory.CreateAnd2(_clock.ClkE, StepWire(2), RamEnable);
 
-        _accEnableOr = ComponentFactory.CreateOr2(StepWire(3), StepWire(6), WireFactory.CreateWire(false, "acc-enable-or"));
-        _accEnableAnd = ComponentFactory.CreateAnd2(_clock.ClkE, _accEnableOr.Output, AccEnable);
-        
-        _generalPurposeRegistersEnableAnd = new IAnd2[WireConstants.ExpectedNumberOfGeneralPurposeRegisters];
-        for (var i = 0; i < WireConstants.ExpectedNumberOfGeneralPurposeRegisters; i++)
-        {
-            _generalPurposeRegistersEnableAnd[i] = ComponentFactory.CreateAnd2(
-                _clock.ClkE,
-                i switch
-                {
-                    0 => StepWire(5),
-                    1 => StepWire(4),
-                    2 => StepWire(4),
-                    _ => WireFactory.OffWire
-                }, 
-                GeneralPurposeRegistersEnable[i]);
-        }
-        
+        // StepWire(3) might be needed?
+        _accEnableAnd = ComponentFactory.CreateAnd2(_clock.ClkE, _irStep6And.Output, AccEnable);
+
+        _generalPurposeRegistersEnableOr = WireConstants.ExpectedNumberOfGeneralPurposeRegisters
+            .InitArray<IOr2>()
+            .Fill(i =>
+                ComponentFactory.CreateOr2(
+                    _registerAEnableDecoderAnd[i].Output,
+                    _registerBEnableDecoderAnd[i].Output,
+                    GeneralPurposeRegistersEnable[i]));
+
         // Sets
         _irSetAnd = ComponentFactory.CreateAnd2(_clock.ClkS, StepWire(2), IrSet);
 
@@ -135,24 +205,11 @@ public class CentralProcessingUnit : PartsBase, ICentralProcessingUnit
         
         _iarSetAnd = ComponentFactory.CreateAnd2(_clock.ClkS, StepWire(3), IarSet);
 
-        _accSetOr = ComponentFactory.CreateOr2(StepWire(1), StepWire(5), WireFactory.CreateWire(false, "acc-set-or"));
-        _accSetAnd = ComponentFactory.CreateAnd2(_clock.ClkS, _accSetOr.Output, AccSet);
+        // StepWire(1) might be needed?
+        _accSetAnd = ComponentFactory.CreateAnd2(_clock.ClkS, _irStep5And.Output, AccSet);
 
         _ramSetAnd = ComponentFactory.CreateAnd2(_clock.ClkS, StepWire(5), RamSet);
-        _tmpSetAnd = ComponentFactory.CreateAnd2(_clock.ClkS, StepWire(4), TmpSet);
-        
-        _generalPurposeRegistersSetAnd = new IAnd2[WireConstants.ExpectedNumberOfGeneralPurposeRegisters];
-        for (var i = 0; i < WireConstants.ExpectedNumberOfGeneralPurposeRegisters; i++)
-        {
-            _generalPurposeRegistersSetAnd[i] = ComponentFactory.CreateAnd2(
-                _clock.ClkS, 
-                i switch
-                {
-                    0 => StepWire(6),
-                    _ => WireFactory.OffWire
-                }, 
-                GeneralPurposeRegistersSet[i]);
-        }
+        _tmpSetAnd = ComponentFactory.CreateAnd2(_clock.ClkS, _irStep4And.Output, TmpSet);
     }
 
     public IWire<bool> Bus1 => StepWire(1);
@@ -188,16 +245,29 @@ public class CentralProcessingUnit : PartsBase, ICentralProcessingUnit
         _clock.Update();
         _stepper.Update();
         
+        _aluOutputAnds.Update();
+        _irAnd.Update();
+        _irNot.Update();
+        
+        _irStep4And.Update();
+        _irStep5And.Update();
+        _irStep6And.Update();
+        
+        _registerAEnable2X4Decoder.Update();
+        _registerAEnableDecoderAnd.Update();
+        
+        _registerBEnable2X4Decoder.Update();
+        _registerBEnableDecoderAnd.Update();
+        
+        _registerBSet2X4Decoder.Update();
+        _registerBSetDecoderAnd.Update();
+
         _iarEnableAnd.Update();
         _ramEnableAnd.Update();
-        
-        _accEnableOr.Update();
+
         _accEnableAnd.Update();
 
-        foreach (var generalPurposeRegisterEnableAnd in _generalPurposeRegistersEnableAnd)
-        {
-            generalPurposeRegisterEnableAnd.Update();
-        }
+        _generalPurposeRegistersEnableOr.Update();
         
         _irSetAnd.Update();
         
@@ -206,16 +276,10 @@ public class CentralProcessingUnit : PartsBase, ICentralProcessingUnit
         
         _iarSetAnd.Update();
         
-        _accSetOr.Update();
         _accSetAnd.Update();
         
         _ramSetAnd.Update();
         _tmpSetAnd.Update();
-        
-        foreach (var generalPurposeRegisterSetAnd in _generalPurposeRegistersSetAnd)
-        {
-            generalPurposeRegisterSetAnd.Update();
-        }
     }
 
     /// <summary>
