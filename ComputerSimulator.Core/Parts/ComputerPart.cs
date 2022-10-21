8418d6688f8c1007c95cc64f04f7dcb1bc3964fa
Adam Brewer <Adam.Brewer@datapath.co.uk>
2022-10-21 14:45:25 +0100

    remove extra code to start computer tests

--- ComputerSimulator.Core/Parts/ComputerPart.cs
+++ ComputerSimulator.Core/Parts/ComputerPart.cs
@@ -11,26 +11,12 @@ public interface IComputerPart : IPart
 public class ComputerPart : PartsBase, IComputerPart
 {
     private readonly ICentralProcessingUnit _cpu;
-    private readonly IBus _bus;
-    private readonly IRegister[] _registers;
-    private readonly IRam _ram;
-    private readonly IRegister _instructionRegister;
-    private readonly IRegister _instructionAddressRegister;
-    private readonly IRegister _tmpRegister;
-    private readonly IBus1 _bus1;
-    private readonly IArithmeticLogicUnit _alu;
-    private readonly IRegister _acc;
 
     public ComputerPart(
         IComponentFactory componentFactory,
         IWireFactory wireFactory)
         : base(componentFactory, wireFactory)
     {
-        _bus = WireFactory.CreateBus("bus");
-
-        var irSet = WireFactory.CreateWire(false, "ir-set");
-        _instructionRegister = ComponentFactory.CreateRegister(irSet, WireFactory.PowerWire, _bus, WireFactory.CreateGroup(false, "ir-output"));
-
         _cpu = ComponentFactory.CreateCentralProcessingUnit(
             WireFactory.CreateWire(false, "bus1"),
             WireFactory.CreateSetEnableWire(false, "iar"),
@@ -41,7 +27,7 @@ public class ComputerPart : PartsBase, IComputerPart
             WireFactory.CreateOp("op"),
             WireFactory.CreateWire(false, "mar-set"),
             WireFactory.CreateWire(false, "tmp-set"),
-            irSet,
+            WireFactory.CreateWire(false, "ir-set"),
             WireFactory.CreateWire(false, "flags-set"),
             WireFactory.CreateWire(false, "carry-in-tmp"),
             WireFactory.CreateWire(false, "io-input-output"),
@@ -49,33 +35,6 @@ public class ComputerPart : PartsBase, IComputerPart
             WireFactory.CreateGroup(false, "instruction-register"),
             WireFactory.CreateCaez(false, "caez")
         );
-
-        _instructionAddressRegister = ComponentFactory.CreateRegister(_cpu.Iar.Set, _cpu.Iar.Enable, _bus, _bus);
-        
-        _registers = new IRegister[WireConstants.ExpectedNumberOfGeneralPurposeRegisters];
-        for (var i = 0; i < WireConstants.ExpectedNumberOfGeneralPurposeRegisters; i++)
-        {
-            _registers[i] = ComponentFactory.CreateRegister(_cpu.GeneralPurposeRegisters[i].Set, _cpu.GeneralPurposeRegisters[i].Enable, _bus, _bus);
-        }
-
-        _ram = ComponentFactory.CreateRam(_cpu.MarSet, _bus, _cpu.Ram.Set, _cpu.Ram.Enable, _bus);
-
-        _tmpRegister = ComponentFactory.CreateRegister(_cpu.TmpSet, WireFactory.PowerWire, _bus, WireFactory.CreateGroup(false, "tmp-output"));
-
-        _bus1 = ComponentFactory.CreateBus1(_cpu.Bus1, _tmpRegister.Outputs, WireFactory.CreateGroup(false, "bus1-output"));
-
-        _alu = ComponentFactory.CreateArithmeticLogicUnit(
-            _bus,
-            _bus1.Outputs,
-            WireFactory.OffWire,
-            _cpu.Op,
-            WireFactory.CreateGroup(false, "alu-output"),
-            WireFactory.OffWire,
-            WireFactory.OffWire,
-            WireFactory.OffWire,
-            WireFactory.OffWire);
-
-        _acc = ComponentFactory.CreateRegister(_cpu.Acc.Set, _cpu.Acc.Enable, _alu.Outputs, _bus);
     }
 
     public void Update()
--- ComputerSimulator.IntegrationTests/Parts/ComputerPartTests.cs
+++ ComputerSimulator.IntegrationTests/Parts/ComputerPartTests.cs
@@ -3,6 +3,7 @@ using NUnit.Framework;
 
 namespace ComputerSimulator.IntegrationTests.Parts;
 
+[TestFixture]
 public class ComputerPartTests : IntegrationTestBase
 {
     private IComputerPart _sut = null!;
@@ -11,5 +12,18 @@ public class ComputerPartTests : IntegrationTestBase
     public void SetUp()
     {
         _sut = ComponentFactory.CreateComputerPart();
+    }
+
+    private void PerformStep(int steps = 1)
+    {
+        for (var i = 0; i < steps; i++)
+        {
+            _sut.Update();
+        }
+    }
+
+    private void PerformFullStep(int fullSteps = 1)
+    {
+        PerformStep(fullSteps * 4);
     }
 }
