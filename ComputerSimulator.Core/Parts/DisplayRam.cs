245b26f688f10143864627b1713623ee521982cd
Adam Brewer <adam@adambrewer.co.uk>
2022-11-08 11:05:48 +0000

    create display ram class

---
+++ ComputerSimulator.Core/Parts/DisplayRam.cs
@@ -0,0 +1,21 @@
+﻿using ComputerSimulator.Core.Factories;
+
+namespace ComputerSimulator.Core.Parts;
+
+public interface IDisplayRam : IPart
+{
+}
+
+public class DisplayRam : PartsBase, IDisplayRam
+{
+    public DisplayRam(
+        
+        IComponentFactory componentFactory, IWireFactory wireFactory) : base(componentFactory, wireFactory)
+    {
+    }
+
+    public void Update()
+    {
+        throw new NotImplementedException();
+    }
+}
