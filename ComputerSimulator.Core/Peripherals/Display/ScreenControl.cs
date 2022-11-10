db5b2f8d800c0aa2f09b352d07099ab99dc465df
Adam Brewer <Adam.Brewer@datapath.co.uk>
2022-11-10 16:02:11 +0000

    create screen control

---
+++ ComputerSimulator.Core/Peripherals/Display/ScreenControl.cs
@@ -0,0 +1,18 @@
+﻿using ComputerSimulator.Core.Parts;
+
+namespace ComputerSimulator.Core.Peripherals.Display;
+
+public interface IScreenControl : IPart
+{
+    IWireGroup<bool> Inputs { get; }
+    
+    IWire<bool> DisplayRamEnable { get; }
+}
+
+public class ScreenControl : IScreenControl
+{
+    public void Update()
+    {
+        throw new NotImplementedException();
+    }
+}
