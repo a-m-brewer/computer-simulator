201b0433c4792fa9c30b1ac9f7a689784ffab660
Adam Brewer <Adam.Brewer@datapath.co.uk>
2022-11-10 16:20:05 +0000

    Created scafolding for IO Bus Control

---
+++ ComputerSimulator.Core/Peripherals/Display/IoBusControl.cs
@@ -0,0 +1,46 @@
+﻿using ComputerSimulator.Core.Factories;
+using ComputerSimulator.Core.Parts;
+
+namespace ComputerSimulator.Core.Peripherals.Display;
+
+public interface IIoBusControl : IPart
+{
+    IBus CpuBus { get; }
+    
+    IWireGroup<bool> DisplayRamSetMarBus { get; }
+    
+    IWire<bool> DisplayRamSetMarSet { get; }
+    
+    IWireGroup<bool> DisplayRamInputBus { get; }
+    
+    IWire<bool> DisplayRamSet { get; }
+}
+
+public class IoBusControl : PartsBase, IIoBusControl
+{
+    public IoBusControl(
+        IBus cpuBus,
+        IWireGroup<bool> displayRamSetMarBus,
+        IWire<bool> displayRamSetMarSet,
+        IWireGroup<bool> displayRamInputBus,
+        IWire<bool> displayRamSet,
+        IComponentFactory componentFactory, IWireFactory wireFactory) : base(componentFactory, wireFactory)
+    {
+        CpuBus = cpuBus;
+        DisplayRamSetMarBus = displayRamSetMarBus;
+        DisplayRamSetMarSet = displayRamSetMarSet;
+        DisplayRamInputBus = displayRamInputBus;
+        DisplayRamSet = displayRamSet;
+    }
+
+    public IBus CpuBus { get; }
+    public IWireGroup<bool> DisplayRamSetMarBus { get; }
+    public IWire<bool> DisplayRamSetMarSet { get; }
+    public IWireGroup<bool> DisplayRamInputBus { get; }
+    public IWire<bool> DisplayRamSet { get; }
+    
+    public void Update()
+    {
+        throw new NotImplementedException();
+    }
+}
--- ComputerSimulator.Core/Peripherals/Display/ScreenControl.cs
+++ ComputerSimulator.Core/Peripherals/Display/ScreenControl.cs
@@ -1,16 +1,59 @@
-﻿using ComputerSimulator.Core.Parts;
+﻿using ComputerSimulator.Core.Factories;
+using ComputerSimulator.Core.Parts;
 
 namespace ComputerSimulator.Core.Peripherals.Display;
 
 public interface IScreenControl : IPart
 {
-    IWireGroup<bool> Inputs { get; }
+    IWireGroup<bool> DisplayRamEnableMarBus { get; }
+
+    IWire<bool> DisplayRamEnableMarSet { get; }
+    
+    IWireGroup<bool> DisplayRamOutputBus { get; }
     
     IWire<bool> DisplayRamEnable { get; }
+    
+    IWire<bool> Clock { get; }
+
+    IWire<bool> HorizontalPosition { get; }
+
+    IWire<bool> VerticalPosition { get; }
+
+    IWire<bool> Brightness { get; }
 }
 
-public class ScreenControl : IScreenControl
+public class ScreenControl : PartsBase, IScreenControl
 {
+    public ScreenControl(
+        IWireGroup<bool> displayRamEnableMarBus,
+        IWire<bool> displayRamEnableMarSet,
+        IWireGroup<bool> displayRamOutputBus, 
+        IWire<bool> displayRamEnable,
+        IWire<bool> clock,
+        IWire<bool> horizontalPosition,
+        IWire<bool> verticalPosition, 
+        IWire<bool> brightness,
+        IComponentFactory componentFactory, IWireFactory wireFactory) : base(componentFactory, wireFactory)
+    {
+        DisplayRamEnableMarBus = displayRamEnableMarBus;
+        DisplayRamEnableMarSet = displayRamEnableMarSet;
+        DisplayRamOutputBus = displayRamOutputBus;
+        DisplayRamEnable = displayRamEnable;
+        Clock = clock;
+        HorizontalPosition = horizontalPosition;
+        VerticalPosition = verticalPosition;
+        Brightness = brightness;
+    }
+
+    public IWireGroup<bool> DisplayRamEnableMarBus { get; }
+    public IWire<bool> DisplayRamEnableMarSet { get; }
+    public IWireGroup<bool> DisplayRamOutputBus { get; }
+    public IWire<bool> DisplayRamEnable { get; }
+    public IWire<bool> Clock { get; }
+    public IWire<bool> HorizontalPosition { get; }
+    public IWire<bool> VerticalPosition { get; }
+    public IWire<bool> Brightness { get; }
+    
     public void Update()
     {
         throw new NotImplementedException();
